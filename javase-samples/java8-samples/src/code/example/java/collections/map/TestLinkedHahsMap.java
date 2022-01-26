583cd10fbd26b8ffc20c8b1c6a775cb888223a16
wangliang <ly-wangliang@szlanyou.com>
2022-01-26 18:03:17 +0800

    111

---
+++ javase-samples/java8-samples/src/code/example/java/collections/map/TestLinkedHahsMap.java
@@ -0,0 +1,21 @@
+package code.example.java.collections.map;
+
+import java.util.LinkedHashMap;
+
+import code.example.java.collections.Hero;
+
+public class TestLinkedHahsMap {
+	public static void main(String[] args) {
+		LinkedHashMap<String, Hero> map = new LinkedHashMap<>();
+		
+		map.put("AAA", new Hero("1", "NAME"));
+		map.put("VVA", new Hero("1", "NAME"));
+		
+		
+		
+		
+		
+		
+		
+	}
+}
