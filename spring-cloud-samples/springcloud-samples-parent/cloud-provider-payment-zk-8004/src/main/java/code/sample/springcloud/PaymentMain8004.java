f4cba69775a2b5be32fcd4ffb1fd098644d12ff4
line <vonlinee@163.com>
2022-02-21 23:39:38 +0800

    springcloud zookeeper

---
+++ spring-cloud-samples/springcloud-samples-parent/cloud-provider-payment-zk-8004/src/main/java/code/sample/springcloud/PaymentMain8004.java
@@ -0,0 +1,51 @@
+package code.sample.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
+/*
+
+解决方法
+https://blog.csdn.net/renkai721/article/details/112257894
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+// springboot2启动项目报错，应该是数据库连接的问题，导致无法启动
+
+// 第一种，项目不需要连接数据库，启动报错
+// 只要在将@SpringBootApplication修改为@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+// 就可以启动的时候不需要连接数据库。
+
+// 第2种，需要连接数据库，启动报错
+
+// #在application.properties/或者application.yml文件中没有添加数据库配置信息.
+//         spring:
+//         datasource:
+//         url: jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&useSSL=false
+//         username: root
+//         password: 123456
+//         driver-class-name: com.mysql.jdbc.Driver
