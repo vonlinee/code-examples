3e797b0e4c6592e6348810c5596b6ff5ded88b02
line <vonlinee@163.com>
2022-02-23 23:53:50 +0800

    spring samples

--- py-utils/workflow/main.py
+++ py-utils/workflow/main.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # 默认待处理文本由文本文件提供
+import json
+import random  # 随机数
 
 import chardet  # 编码检测
 import xlrd  # 读Excel
 import xlwt  # 写Excel
-import random  # 随机数
-import json
+
 
 def detect_qt_environment():
     pass
@@ -19,6 +20,8 @@ OUTPUT_TXT_FILE_PATH = "./output.txt"
 
 # 名称(1)	     值(2)	        类型(3)
 # 编码  dlrOrderCode   String
+
+
 def read_excel(excel_path, api_method_name="", txt_path=None) -> str:
     excel_data = xlrd.open_workbook(excel_path)
     table = excel_data.sheet_by_index(0)
@@ -27,12 +30,13 @@ def read_excel(excel_path, api_method_name="", txt_path=None) -> str:
     l = []
     for i in range(0, row_count):  # 第一行为表头，不记录,0表示第一行
         row = table.row_values(i, start_colx=0, end_colx=None)
-        graphql_column_str = s1 + "'" + row[1] + "', " + "'" + row[0] + "'"+ s2
+        graphql_column_str = s1 + "'" + \
+            row[1] + "', " + "'" + row[0] + "'" + s2
         print(graphql_column_str)
     return "".join(l)
 
 
-def make_graphql_field(template_path: str, field_type: str, field_name: str, output_dir: str=None) -> None:
+def make_graphql_field(template_path: str, field_type: str, field_name: str, output_dir: str = None) -> None:
     """[summary]
     输出GraphQL的Field Schema文本
     Args:
@@ -42,14 +46,17 @@ def make_graphql_field(template_path: str, field_type: str, field_name: str, out
         output_dir (RELATIVE_OUTPUT_DIR): [description]
     """
     if output_dir is None:
-        output_dir = RELATIVE_OUTPUT_DIR # "./output/"
-    content = field_type + " " + field_name + " {" + read_excel(template_path) + "\n}"
+        output_dir = RELATIVE_OUTPUT_DIR  # "./output/"
+    content = field_type + " " + field_name + \
+        " {" + read_excel(template_path) + "\n}"
     output_path = output_dir + "1.txt"
-    with open(output_path, "w", encoding= 'utf-8') as f:
+    with open(output_path, "w", encoding='utf-8') as f:
         f.write(content)
         f.flush()
 
 # USER_NAME -> userName
+
+
 def snake_to_camel_style(snake_str: str, upper_first=False) -> str:
     """[summary]
     Args:
@@ -67,6 +74,7 @@ def snake_to_camel_style(snake_str: str, upper_first=False) -> str:
         return s[0].upper() + s[1:]
     else:
         return s[0].lower() + s[1:]
+
 
 def camel_to_snake_style(camel_str: str, lower_all=False) -> str:
     """[summary]
@@ -86,10 +94,12 @@ def camel_to_snake_style(camel_str: str, lower_all=False) -> str:
         return result.lower()
     return result.upper()
 
+
 def pascal_case_to_snake_case(camel_case: str):
     import re
     snake_case = re.sub(r"(?P<key>[A-Z])", r"_\g<key>", camel_case)
     return snake_case.lower().strip('_')
+
 
 def snake_to_pascal_case(snake_case: str, upper_first=False):
     """蛇形转大驼峰（帕斯卡）"""
@@ -98,6 +108,7 @@ def snake_to_pascal_case(snake_case: str, upper_first=False):
     if upper_first:
         return result[0].upper() + result[1:]
     return result[0].lower() + result[1:]
+
 
 def generate_dubbo_xml_config(interfaceName: str, serviceId: str) -> str:
     """[summary]
@@ -111,23 +122,26 @@ def generate_dubbo_xml_config(interfaceName: str, serviceId: str) -> str:
     """
     return '<dubbo:service interface="{interfaceName}" ref="{serviceId}"/>'.format(interfaceName=interfaceName, serviceId=serviceId)
 
+
 def method(param_list: dict, return_type: str) -> str:
     pass
+
 
 def create_graphqls_content():
     input_prefix = "Input"
     suffix = "Ext"
     result_prefix = "ListResult"
     default_template_dir = "./ExcelTemplate/"
-    make_graphql_field(default_template_dir + r"字段描述模板.xlsx", "type", "VeMktActInfoActMode")
+    make_graphql_field(default_template_dir + r"字段描述模板.xlsx",
+                       "type", "VeMktActInfoActMode")
 
 
 def batch_snake_to_camel_style(
-    input_source=1,
-    output_target= 0,
-    *args, 
-    input_file_path=INPUT_TXT_FILE_PATH,
-    output_file_path=OUTPUT_TXT_FILE_PATH):
+        input_source=1,
+        output_target=0,
+        *args,
+        input_file_path=INPUT_TXT_FILE_PATH,
+        output_file_path=OUTPUT_TXT_FILE_PATH):
     if input_source == 0:  # 参数输入
         result_list = []
         for arg in args:
@@ -154,11 +168,11 @@ def batch_snake_to_camel_style(
 
 
 def batch_camel_to_snake_style(
-    input_source=0, 
-    output_target= 0, 
-    *args, 
-    input_file_path=INPUT_TXT_FILE_PATH,
-    output_file_path=OUTPUT_TXT_FILE_PATH):
+        input_source=0,
+        output_target=0,
+        *args,
+        input_file_path=INPUT_TXT_FILE_PATH,
+        output_file_path=OUTPUT_TXT_FILE_PATH):
     if input_source == 0:   # 函数传参
         result_list = []
         for arg in args:
@@ -169,7 +183,7 @@ def batch_camel_to_snake_style(
         if output_target == 1:
             with open(output_file_path, 'w') as f:
                 f.writelines(result_list)
-    elif input_source == 1: # 文本文件
+    elif input_source == 1:  # 文本文件
         result_list = []
         with open(input_file_path, 'r') as f:
             lines = f.readlines()
@@ -185,8 +199,8 @@ def batch_camel_to_snake_style(
 
 
 def remove_blank_lines(
-    input_file: str = INPUT_TXT_FILE_PATH,
-    output_file: str = OUTPUT_TXT_FILE_PATH) -> None:
+        input_file: str = INPUT_TXT_FILE_PATH,
+        output_file: str = OUTPUT_TXT_FILE_PATH) -> None:
     """[remove_blank_lines]
     Args:
         input_file (str, optional): [description]. Defaults to './input.txt'.
@@ -196,10 +210,9 @@ def remove_blank_lines(
     with open(input_file, 'r') as input:
         line = input.readline()
         if not line.replace(' ', '') == '':
-             new_lines.append(line)
+            new_lines.append(line)
     with open(output_file, 'w') as output:
         output.writelines(line for line in new_lines)
-    
 
 
 def graphql_type_to_excel(graphqls_file: str) -> None:
@@ -210,7 +223,7 @@ def graphql_type_to_excel(graphqls_file: str) -> None:
     """
     with open(graphqls_file) as graphqls_file:
         lines = graphqls_file.readlines()
-        line_no = 0 # 行号
+        line_no = 0  # 行号
         column_list = []
         column_info = {}
         flag = 1
@@ -219,13 +232,13 @@ def graphql_type_to_excel(graphqls_file: str) -> None:
             line_no = line_no + 1
             if line_no == 1:
                 strings = line.replace('\n', '').split(" ")
-            else: # 去除第一行
-                if line_no % 2 == 0: # 字段描述
+            else:  # 去除第一行
+                if line_no % 2 == 0:  # 字段描述
                     line = line.strip().replace('\n', '')
                     len = len(line)
                     column_info['column_description'] = line[1:len]
                     flag = 2
-                if line_no % 2 == 1: # 字段名：字段类型
+                if line_no % 2 == 1:  # 字段名：字段类型
                     column = line.strip().split(':')
                     column_info['column_name'] = column[0]
                     column_info['column_type'] = column[1]
@@ -233,9 +246,10 @@ def graphql_type_to_excel(graphqls_file: str) -> None:
             if flag == 3:
                 column_list.append(column_info)
                 flag = 1
-                column_info.clear() # 清空字段信息表
+                column_info.clear()  # 清空字段信息表
         for item in column_list:
-            print(item['column_description'] + "-" + item['column_name'] + "-" + item['column_type'])     
+            print(item['column_description'] + "-" +
+                  item['column_name'] + "-" + item['column_type'])
 
 
 def format_date_column(column_name: str) -> str:
@@ -264,8 +278,8 @@ def batch_format_date_column() -> None:
 
 
 def split_line_to_nline(
-    input_file: str = INPUT_TXT_FILE_PATH,
-    delimeter=',') -> None:
+        input_file: str = INPUT_TXT_FILE_PATH,
+        delimeter=',') -> None:
     """ 
     Args:
         input_file (str, optional): [input file contains one line ]. Defaults to './input.txt'.
@@ -282,8 +296,8 @@ def split_line_to_nline(
 
 
 def merge_nline_to_line(
-    input_file: str = INPUT_TXT_FILE_PATH,
-    delimeter=',') -> None:
+        input_file: str = INPUT_TXT_FILE_PATH,
+        delimeter=',') -> None:
     """[merge_nline_to_line]
 
     Args:
@@ -298,6 +312,7 @@ def merge_nline_to_line(
         line = line.replace('\n', '').strip()
         new_line.append(line)
     print(delimeter.join(new_line))
+
 
 def generate_sql(template_name='Template1.xlsx') -> None:
     """[summary]
@@ -324,7 +339,8 @@ def lines_to_empty_json(input_file: str = INPUT_TXT_FILE_PATH):
     empty_key_json = {}
     for line in lines:
         empty_key_json[line.strip()] = ""
-    json_str = json.dumps(empty_key_json, sort_keys=True, indent=4, separators=(',', ': '))
+    json_str = json.dumps(empty_key_json, sort_keys=True,
+                          indent=4, separators=(',', ': '))
     print(json_str)
 
 
@@ -335,10 +351,10 @@ def print_random_phone_number(count: int = 1):
     #         '(联通)130', '(联通)131', '(联通)132', '(联通)155', '(联通)156', '(联通)185', '(联通)186',
     #         '(电信)133', '(电信)153', '(电信)180', '(电信)189', ]
     l = ['134', '135', '136', '137', '138', '139', '150',
-        '151', '152', '157', '158', '159', '187', '188',
-        '130', '131', '132', '155', '156', '185', '186',
-        '133', '153', '180', '189',]
-    haom = '' # 空字符串
+         '151', '152', '157', '158', '159', '187', '188',
+         '130', '131', '132', '155', '156', '185', '186',
+         '133', '153', '180', '189', ]
+    haom = ''  # 空字符串
     haoma1 = []    # 空列表
     for i in range(0, count):
         a = random.choice(l)   # 随机选择一个三位数作为手机号前三位
@@ -350,20 +366,26 @@ def print_random_phone_number(count: int = 1):
         haoma1.clear()  # 清空上一次的输出
 
 # 按指定步长递增数字
-def increasing_num_by_step(start: int=0, end: int=20, step: int = 1) -> list:
+
+
+def increasing_num_by_step(start: int = 0, end: int = 20, step: int = 1) -> list:
     nums = []
     for x in range(start, end + 1, step):
         nums.append(x)
     return nums
 
 # 按步长打印数字
+
+
 def print_increasing_num_by_step(
-    start: int=0, end: int=20, step: int = 1) -> list:
+        start: int = 0, end: int = 20, step: int = 1) -> list:
     nums = []
     for x in range(start, end + 1, step):
         print(x)
 
 # 去除每行的首位空格
+
+
 def batch_strip(input_file: str = INPUT_TXT_FILE_PATH):
     lines = None
     with open(input_file, 'r') as f:
@@ -372,7 +394,7 @@ def batch_strip(input_file: str = INPUT_TXT_FILE_PATH):
             print(line.strip())
 
 
-def batch_aligen(aligenment: str= 'L')-> None:
+def batch_aligen(aligenment: str = 'L') -> None:
     """
     文本对齐，默认左对齐
     """
@@ -392,7 +414,7 @@ def batch_aligen(aligenment: str= 'L')-> None:
                 print(line.center(max_len))
 
 
-def batch_upper_first_word()-> None:
+def batch_upper_first_word() -> None:
     lines = []
     with open('./input.txt', 'r') as f:
         read_lines = f.readlines()
@@ -402,20 +424,48 @@ def batch_upper_first_word()-> None:
                 print()
             else:
                 print(line.strip().upper_first())
-
+import os
 
 
 # s = '<if test="${columnValue} !=null and param.taskId !='' ">'\
 # '    and T.TASK_ID = #{param.taskId}' \
 # '</if>'
+def delete_files(file_name: str) -> None:
+    if file_name.endswith('.project') or file_name.endswith('.classpath'):
+        os.remove(file_name)
+        print("删除" + file_name)
+    if file_name.endswith('.iml'):
+        os.remove(file_name)
+        print("删除" + file_name)
 
 
-def delete_project_files() -> None:
-    pass
+def delete_directory(dir: str) -> None:
+    try:
+        if dir.endswith('bin') or dir.endswith('target') or dir.endswith('.settings'):
+            os.removedirs(dir)
+            print("删除" + dir)
+    except OSError as error:
+        print(error.errno)
+
+
+def delete_project_files(root_dir: str) -> None:
+    dirs = []
+    dirs.append(root_dir)
+    while len(dirs) != 0:
+        dir_abs_path = os.getcwd() + dirs.pop()
+        print(dir_abs_path)
+        for filename in os.listdir(dir_abs_path):
+            if os.path.isdir( + filename):
+                print(filename)
+                delete_directory(filename)
+                dirs.append(filename)
+            else:
+                delete_files(filename)
 
 
 if __name__ == '__main__':
-    batch_snake_to_camel_style()
+    delete_project_files(r'D:\\Develop\\Projects\\Github\\code-example')
+
 
 # with open("input.txt", 'r') as f:
 #     result_list = []
@@ -423,12 +473,8 @@ if __name__ == '__main__':
 #     for line in lines:
 #         s = str(line).strip()
 #         camel_style = snake_to_camel_style(s)
-#         camel_style = 'param.' + camel_style 
+#         camel_style = 'param.' + camel_style
 #         print('<if test="' + camel_style + ' !=null and ' + camel_style + ' !='' ">')
 #         print('AND T.' + s + ' = #{ ' + camel_style + ' }")'
 #         print('</if>')
 #         result_list.append()
-
-
-
-
