58f77f5ec4d38e9741a3b5cec6b347d096898570
line <vonlinee@163.com>
2022-03-05 04:42:19 +0800

    111

--- jedis-sample/src/main/java/code/sample/jedis/Test.java
+++ jedis-sample/src/main/java/code/sample/jedis/Test.java
@@ -5,11 +5,13 @@ import redis.clients.jedis.Jedis;
 public class Test {
     public static void main(String[] args) {
         //连接本地的 Redis 服务
-        Jedis jedis = new Jedis("101.33.212.245", 6379);
+        Jedis jedis = new Jedis("localhost", 6379);
         // 如果 Redis 服务设置了密码，需要下面这行，没有就不需要
-        jedis.auth("line@123!!");
         System.out.println("连接成功");
         //查看服务是否运行
         System.out.println("服务正在运行: " + jedis.ping());
+
+        jedis.set("name", "孙允珠");
+        jedis.set("age", "30");
     }
 }
---
+++ trouble-maker/mybatis-generator/src/main/java/io/maker/generator/db/sql/MySQLParser.java
@@ -0,0 +1,14 @@
+package io.maker.generator.db.sql;
+
+import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlOutputVisitor;
+import org.apache.shardingsphere.sql.parser.sql.statement.SQLStatement;
+
+import java.util.List;
+
+public class MySQLParser extends SqlParser {
+
+    public static void main(String[] args) {
+
+    }
+}
