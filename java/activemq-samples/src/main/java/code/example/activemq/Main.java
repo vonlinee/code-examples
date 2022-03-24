793333f96adba22c08c943458b3661e6579d2808
line <vonlinee@163.com>
2022-03-25 00:24:33 +0800

    11

--- java/activemq-samples/src/main/java/code/example/activemq/Main.java
+++ java/activemq-samples/src/main/java/code/example/activemq/Main.java
@@ -1,8 +1,27 @@
 package code.example.activemq;
 
 import javax.jms.JMSException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
-	
-	public static void main(String[] args) throws JMSException {
-	}
+
+    public static void main(String[] args) throws JMSException {
+
+        ArrayList<Map<String, Object>> list = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("k1", "v1");
+            map.put("k2", "v2");
+            list.add(map);
+        }
+
+        list.toString();
+
+        System.out.println(list);
+
+    }
 }
