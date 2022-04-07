19f2d403d0281ed63b87d932dda06677d78347ca
line <vonlinee@163.com>
2022-04-07 11:47:10 +0800

    111

--- java/spring-framework/spring-boot-samples/springboot-db-integration/springboot-dynamic-datasource/src/main/java/sample/dynamic/datasource/config/DataSourceConfiguration.java
+++ java/spring-framework/spring-boot-samples/springboot-db-integration/springboot-dynamic-datasource/src/main/java/sample/dynamic/datasource/config/DataSourceConfiguration.java
@@ -1,9 +1,18 @@
 package sample.dynamic.datasource.config;
 
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan(sqlSessionFactoryRef = "sqlSessionFactoryBean")
 public class DataSourceConfiguration {
 
-
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean() {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setMapperLocations();
+        return bean;
+    }
 }
