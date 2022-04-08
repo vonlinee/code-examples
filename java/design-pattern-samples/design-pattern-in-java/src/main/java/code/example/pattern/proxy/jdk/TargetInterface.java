feff25f5f9591c33b8284e041436ce5307bd73a2
line <vonlinee@163.com>
2022-04-08 23:35:43 +0800

    111

---
+++ java/design-pattern-samples/design-pattern-in-java/src/main/java/code/example/pattern/proxy/jdk/TargetInterface.java
@@ -0,0 +1,5 @@
+package code.example.pattern.proxy.jdk;
+
+public interface TargetInterface {
+    String targetMethod(String param);
+}
--- java/sharding-sphere/sharding-sphere-samples/sharding-jdbc-quickstart/src/main/java/sample/sharding/jdbc/ShardingJdbcApplication.java
+++ java/sharding-sphere/sharding-sphere-samples/sharding-jdbc-quickstart/src/main/java/sample/sharding/jdbc/ShardingJdbcApplication.java
@@ -3,8 +3,9 @@ package sample.sharding.jdbc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("sample.sharding.jdbc.mapper")
 public class ShardingJdbcApplication {
     public static void main(String[] args) {
