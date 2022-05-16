6f2afe4c9c3a48367fb8c4ec218878e697704fb7
line <vonlinee@163.com>
2022-05-16 08:05:01 +0800

    111

---
+++ java/spring-framework/spring-boot-samples/spring-boot-zk-samples/src/main/java/sample/zookeeper/spring/boot/MainApplication.java
@@ -0,0 +1,16 @@
+package sample.zookeeper.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MainApplication {
+	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "false");
+		SpringApplication application = new SpringApplication(MainApplication.class);
+		ConfigurableApplicationContext context = application.run(args);
+		String[] aliases = context.getAliases("spring");
+		System.out.println(aliases);
+	}
+}
