1cdbedf7af9cac676f42c277587c61849ed6b2fa
line <vonlinee@163.com>
2022-05-29 13:12:54 +0800

    111

--- tools/mybatis/mybatis-generator/mybatis-generator-web/src/main/java/io/maker/codegen/mybatis/controller/CodeGenController.java
+++ tools/mybatis/mybatis-generator/mybatis-generator-web/src/main/java/io/maker/codegen/mybatis/controller/CodeGenController.java
@@ -1,4 +1,10 @@
 package io.maker.codegen.mybatis.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/codegen", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CodeGenController {
 }
