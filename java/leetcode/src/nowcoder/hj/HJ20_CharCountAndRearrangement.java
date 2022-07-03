a79a817f77c3380cddd3452b12fb59d3d7de0202
line <vonlinee@163.com>
2022-07-04 04:39:43 +0800

    111

---
+++ java/leetcode/src/nowcoder/hj/HJ20_CharCountAndRearrangement.java
@@ -0,0 +1,12 @@
+package nowcoder.hj;
+
+/**
+ * https://blog.nowcoder.net/n/7f33847b6af84557a66545dc23da04ef
+ */
+public class HJ20_CharCountAndRearrangement {
+
+    public static void main(String[] args) {
+
+    }
+
+}
