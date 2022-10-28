369cc9bfd33b2f9c86f4c570059d6f9d2135ca13
wangliang <wangliang9906@foxmail.com>
2022-10-28 18:48:08 +0800

    111

--- java/spring-framework/spring-security/src/main/java/sample/spring/security/SecurityConfig.java
+++ java/spring-framework/spring-security/src/main/java/sample/spring/security/SecurityConfig.java
@@ -29,9 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// 下面这两行配置表示在内存中配置了两个用户
-		auth.inMemoryAuthentication().withUser("javaboy").roles("admin")
-				.password("$2a$10$OR3VSksVAmCzc.7WeaRPR.t0wyCsIj24k0Bne8iKWV1o.V9wsP8Xe").and().withUser("lisi")
-				.roles("user").password("$2a$10$p1H8iWa8I4.CA.7Z8bwLjes91ZpY.rYREGHQEInNtAp4NzL6PLKxi");
+		// auth.inMemoryAuthentication().withUser("javaboy").roles("admin")
+		// 		.password("$2a$10$OR3VSksVAmCzc.7WeaRPR.t0wyCsIj24k0Bne8iKWV1o.V9wsP8Xe").and().withUser("lisi")
+		// 		.roles("user").password("$2a$10$p1H8iWa8I4.CA.7Z8bwLjes91ZpY.rYREGHQEInNtAp4NzL6PLKxi");
 	}
 
 	@Override
