eedd630c37f5e61eed9b8bb6f1ba03226a9e47c6
line <vonlinee@163.com>
2022-11-06 21:18:36 +0800

    111

--- java/spring-framework/spring-boot-samples/spring-boot-mvc/src/main/java/org/example/springboot/MainApplication.java
+++ java/spring-framework/spring-boot-samples/spring-boot-mvc/src/main/java/org/example/springboot/MainApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ServletComponentScan
+@ImportResource(value = {"classpath:obs.properties"})
 public class MainApplication {
-	public static void main(String[] args) {
-		System.setProperty("spring.devtools.restart.enabled", "false");
-		SpringApplication application = new SpringApplication(MainApplication.class);
-		ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication application = new SpringApplication(MainApplication.class);
+        ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
+    }
 }
