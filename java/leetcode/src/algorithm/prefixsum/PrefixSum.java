30bdb26291b84c939c85a42222a61a9b46cf8561
wangliang <wangliang9906@foxmail.com>
2022-11-06 16:44:08 +0800

    111

---
+++ java/leetcode/src/algorithm/prefixsum/PrefixSum.java
@@ -0,0 +1,13 @@
+package algorithm.prefixsum;
+
+/**
+ * 前缀和
+ * @author Von
+ *
+ */
+public class PrefixSum {
+
+	public static void main(String[] args) {
+		
+	}
+}
--- web/js-samples/test.js
+++ web/js-samples/test.js
@@ -1,23 +1,10 @@
 
 
+var vehicle = function () {
+    
+};
 
 
-var People = function () {
-    this.race = 'Human'
-}
-
-var func1 = function () {
-
-}
-
-console.log(People);
-console.log(func1);
-
-var p = new People()
-
-console.log(p.constructor);
-
-
-
+console.log(vehicle())
 
 
