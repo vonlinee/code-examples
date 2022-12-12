02f4009c99b325e7e186b6fbcbe13d5be9cff2d8
wangliang <wangliang9906@foxmail.com>
2022-12-12 22:03:13 +0800

    编辑器

---
+++ rich-client-develop/jfx-editor/src/main/java/editor/TextEditor.java
@@ -0,0 +1,37 @@
+package editor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.nio.file.Path;
+
+public class TextEditor extends Application {
+    Parent root;
+    static Path path = null;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(TextEditor.class.getResource("MainPage.fxml"));
+        root = loader.load();
+        MainPageController controller = loader.getController();
+        controller.setEditor(this, stage);
+        controller.setPath(path);
+
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("编辑器");
+        stage.getIcons().add(new Image("img/Jeditor.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
