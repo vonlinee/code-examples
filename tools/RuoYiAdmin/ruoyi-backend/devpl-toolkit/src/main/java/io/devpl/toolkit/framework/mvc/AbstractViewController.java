81bf107506735dd5da466c5422ef4cbc6cd280d4
wangliang <wangliang9906@foxmail.com>
2023-01-06 21:40:16 +0800

    no message

--- tools/RuoYiAdmin/ruoyi-backend/devpl-toolkit/src/main/java/io/devpl/toolkit/framework/mvc/AbstractViewController.java
+++ tools/RuoYiAdmin/ruoyi-backend/devpl-toolkit/src/main/java/io/devpl/toolkit/framework/mvc/AbstractViewController.java
@@ -1,6 +1,6 @@
 package io.devpl.toolkit.framework.mvc;
 
-import javafx.event.EventDispatchChain;
+import javafx.event.*;
 
 public abstract class AbstractViewController implements ViewController {
 
@@ -10,5 +10,40 @@ public abstract class AbstractViewController implements ViewController {
     @Override
     public EventDispatchChain buildEventDispatchChain(EventDispatchChain tail) {
         return viewControllerDispatcherChain;
+    }
+
+    @Override
+    public EventDispatcher getEventDispatcher() {
+        return null;
+    }
+
+    /**
+     * Registers an event filter to this node. The filter is called when the
+     * node receives an {@code Event} of the specified type during the capturing
+     * phase of event delivery.
+     * @param <T>         the specific event class of the filter
+     * @param eventType   the type of the events to receive by the filter
+     * @param eventFilter the filter to register
+     * @throws NullPointerException if the event type or filter is null
+     */
+    public final <T extends Event> void addEventFilter(
+            final EventType<T> eventType,
+            final EventHandler<? super T> eventFilter) {
+
+    }
+
+    /**
+     * Registers an event handler to this node. The handler is called when the
+     * node receives an {@code Event} of the specified type during the bubbling
+     * phase of event delivery.
+     * @param <T>          the specific event class of the handler
+     * @param eventType    the type of the events to receive by the handler
+     * @param eventHandler the handler to register
+     * @throws NullPointerException if the event type or handler is null
+     */
+    public final <T extends Event> void addEventHandler(
+            final EventType<T> eventType,
+            final EventHandler<? super T> eventHandler) {
+
     }
 }
--- tools/RuoYiAdmin/ruoyi-backend/devpl-toolkit/src/main/java/io/devpl/toolkit/framework/mvc/ViewController.java
+++ tools/RuoYiAdmin/ruoyi-backend/devpl-toolkit/src/main/java/io/devpl/toolkit/framework/mvc/ViewController.java
@@ -1,7 +1,13 @@
 package io.devpl.toolkit.framework.mvc;
 
+import javafx.event.EventDispatcher;
 import javafx.event.EventTarget;
 
 public interface ViewController extends EventTarget {
 
+    /**
+     * 事件传递
+     * @return EventDispatcher
+     */
+    EventDispatcher getEventDispatcher();
 }
