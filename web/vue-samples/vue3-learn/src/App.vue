1563a76fca830ad0842ce8e995883b12923034ed
line <vonlinee@163.com>
2023-04-28 00:06:57 +0800

    no message

---
+++ web/vue-samples/vue3-learn/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    {{ student.age }}
+  </div>
+
+  <button @click="show">年龄</button>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+import { onBeforeMount, onMounted } from "vue";
+
+export default {
+  name: "App",
+  components: {},
+  setup() {
+    onBeforeMount(() => {
+      console.log("V3 beforeMount!");
+    });
+
+    let m = reactive({
+      name: "ll",
+      age: 23,
+    });
+
+    console.log(m)
+
+    onMounted(() => {
+      console.log("V3 mounted!");
+    });
+
+    let student = ref({
+      name: "zs",
+      age: 20,
+    });
+    function show() {
+      student.value.age++;
+    }
+
+    return {
+      student,
+      show,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
