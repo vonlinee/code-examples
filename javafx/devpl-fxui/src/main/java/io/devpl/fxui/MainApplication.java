d9c68c749d573419258aacf626c018758987e58d
line <vonlinee@163.com>
2023-08-26 19:42:44 +0800

    11

---
+++ javafx/devpl-fxui/src/main/java/io/devpl/fxui/MainApplication.java
@@ -0,0 +1,29 @@
+package io.devpl.fxui;
+
+import io.devpl.fxui.view.NavigationPane;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
+
+        final VBox root = fxmlLoader.load();
+        root.getChildren().add(new NavigationPane());
+        Scene scene = new Scene(root, 620, 640);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ javafx/devpl-fxui/src/main/java/io/devpl/fxui/view/NavigationMenu.java
@@ -0,0 +1,10 @@
+package io.devpl.fxui.view;
+
+import javafx.scene.control.TreeView;
+
+/**
+ * 导航菜单栏
+ */
+public class NavigationMenu extends TreeView<String> {
+
+}
