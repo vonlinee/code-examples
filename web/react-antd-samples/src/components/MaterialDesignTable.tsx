0d345ffcfc754b693d0b0aa431a680e5670d6631
line <vonlinee@163.com>
2023-11-16 22:30:24 +0800

    1

--- web/react-antd-samples/src/components/MaterialDesignTable.tsx
+++ web/react-antd-samples/src/components/MaterialDesignTable.tsx
@@ -23,10 +23,10 @@ export default function App() {
         accessorKey: "name", //simple recommended way to define a column
         header: "Name",
         muiTableHeadCellProps: { sx: { color: "green" } }, //custom props
-        Cell: (param: any) => <strong>{param.renderedCellValue}</strong> //optional custom cell render
+        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong> //optional custom cell render
       },
       {
-        accessorFn: (row : any) => row.age, //alternate way
+        accessorFn: (row) => row.age, //alternate way
         id: "age", //id required if you use accessorFn instead of accessorKey
         header: "Age",
         Header: <i style={{ color: "red" }}>Age</i> //optional custom markup
