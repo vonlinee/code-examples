0545861e302e0f8861ddbd78acff71554a9dbf74
line <vonlinee@163.com>
2024-01-28 22:59:32 +0800

    1

--- web/vue-samples/vue2-learn/src/App.vue
+++ web/vue-samples/vue2-learn/src/App.vue
@@ -1,99 +1,74 @@
 <template>
-  <div id="app">
-    <!-- <button @click="show">show</button>
-    <HelloWorld msg="message" :callbackFunction="showValue" ref="hw" /> -->
+  <dragTreeTable :data="treeData" :onDrag="onTreeDataChange"></dragTreeTable>
 
-    <!-- <VueCM></VueCM> -->
-    <p></p>
-    <p></p>
-
-    <!-- Table
-    <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
-
-    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
-      <VueCM></VueCM>
-    </el-dialog> -->
-    <input v-focus />
-  </div>
 </template>
 
 <script>
-// import func from "vue-editor-bridge";
-// import HelloWorld from './components/HelloWorld.vue'
-// import CodeMirrorEditor from './components/cm/CodeMirrorEditor.vue'
-// import VueCM from './components/cm/VueCM.vue'
+import dragTreeTable from "drag-tree-table";
+
 export default {
-  name: "App",
-  components: {
-    // CodeMirrorEditor,
-    // VueCM
-  },
-  directives: {
-    focus: {
-      mounted: (el) => el.focus(),
-    },
-  },
-  data: function () {
+  name: "app",
+  data() {
     return {
-      parentData: "111",
-      gridData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-      ],
-      dialogTableVisible: false,
-      dialogFormVisible: false,
-      form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
-      },
-      formLabelWidth: "120px",
+      treeData: {
+        columns: [
+          {
+            title: "名称",
+            field: "name"
+          }, {
+            title: "名称",
+            field: "name"
+          }
+        ],
+        lists: [
+          {
+            "id": 40,
+            "parent_id": 0,
+            "order": 0,
+            "name": "动物类",
+            "uri": "/masd/ds",
+            "open": true,
+            "lists": []
+          }, {
+            "id": 5,
+            "parent_id": 0,
+            "order": 1,
+            "name": "昆虫类",
+            "uri": "/masd/ds",
+            "open": true,
+            "isShowCheckbox": false, // '如果设置了type=checkbox,但是想控制某些行不显示CheckBox，可以用此属性'
+            "lists": [
+              {
+                "id": 12,
+                "parent_id": 5,
+                "open": true,
+                "order": 0,
+                "name": "蚂蚁",
+                "uri": "/masd/ds",
+                "lists": []
+              }
+            ]
+          },
+          {
+            "id": 19,
+            "parent_id": 0,
+            "order": 2,
+            "name": "植物类",
+            "uri": "/masd/ds",
+            "open": true,
+            "lists": []
+          }
+        ]
+      }
     };
   },
+  components: {
+    dragTreeTable
+  },
   methods: {
-    show() {
-      console.log(this.$refs.hw);
-    },
-    showValue(val) {
-      console.log("子组件调用回调函数:", val);
-    },
-  },
-  mounted() {
-    // this.$refs.hw.$on('event', function (val) {
-    //   console.log(val)
-    // })
-  },
+    onTreeDataChange(list) {
+      this.treeData.lists = list;
+    }
+  }
 };
 </script>
-
-<style>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #135ea9;
-  margin-top: 60px;
-}
-</style>
