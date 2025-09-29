0e37465e46ded25eaed43a691260837615b88d36
vonlinee <vonlinee@163.com>
2025-09-29 23:02:48 +0800

    一些PDF

--- bigdata/spark/spark-quickstart/src/main/java/SparkExample.java
+++ bigdata/spark/spark-quickstart/src/main/java/SparkExample.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SparkExample {
-    public static void main(String[] args) {
-        SparkConf conf = new SparkConf()
-                .setAppName("Spark Example")
-                .setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
-        JavaRDD<Integer> rdd = sc.parallelize(data);
-    }
+  public static void main(String[] args) {
+    SparkConf conf = new SparkConf()
+      .setAppName("Spark Example")
+      .setMaster("local[*]");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+    JavaRDD<Integer> rdd = sc.parallelize(data);
+  }
 }
